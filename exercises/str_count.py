6d961c237be8980c87d4a51799a1f900b476fc66
Chaudhry Junaid Anwar <c.junaid@bboxx.co.uk>
2022-11-03 10:14:59 +0500

    with constraints

--- exercises/str_count.py
+++ exercises/str_count.py
@@ -1,5 +1,8 @@
 s = input("Enter a string? ")
+while not s:
+    s = input("Please enter a non-empty string? ")
 count = 0
 for c in s:
     count += 1
-print(f"{s} has {count} characters.")
+count2 = len(s)
+print(f"{s} has {count=} {count2=} characters.")
